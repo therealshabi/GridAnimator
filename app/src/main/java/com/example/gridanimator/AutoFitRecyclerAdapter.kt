c68679766318e0b185ab8c57b6ced6aa1a98cdcd
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-24 16:57:59 +0530

    Use configuration to create grid

--- app/src/main/java/com/example/gridanimator/AutoFitRecyclerAdapter.kt
+++ app/src/main/java/com/example/gridanimator/AutoFitRecyclerAdapter.kt
@@ -8,20 +8,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.DecelerateInterpolator
+import android.widget.FrameLayout
+import androidx.core.view.setMargins
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.grid_element.view.*
 
-class AutoFitRecyclerAdapter(private val gridElements: ArrayList<Int>) :
+class AutoFitRecyclerAdapter(
+    private val gridElements: ArrayList<Int> = ArrayList()
+) :
     RecyclerView.Adapter<AutoFitRecyclerAdapter.AutoFitViewHolder>() {
 
     inner class AutoFitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    fun getElements(): ArrayList<Int> {
+        return this.getElements()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AutoFitViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.grid_element, parent, false)
         val params = UIUtilility.dpToPixels(parent.context, GridActivity.elementSize.toFloat())
-        view.layoutParams = ViewGroup.LayoutParams(params.toInt(), params.toInt())
+        val layoutParams = FrameLayout.LayoutParams(params.toInt(), params.toInt())
+        layoutParams.setMargins(
+            UIUtilility.dpToPixels(
+                parent.context, GridActivity.elementSpacing.toFloat()
+            ).toInt()
+        )
+        view.layoutParams = layoutParams
         return AutoFitViewHolder(view)
     }
 
--- app/src/main/java/com/example/gridanimator/GridActivity.kt
+++ app/src/main/java/com/example/gridanimator/GridActivity.kt
@@ -1,7 +1,6 @@
 package com.example.gridanimator
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
@@ -11,10 +10,10 @@ import kotlinx.android.synthetic.main.activity_grid.*
 class GridActivity : AppCompatActivity() {
 
     companion object {
-        var elementSize = 80
-        var elementSpacing = 2
-        var elementAnimationDelay = 250
-        var numOfElements = 20
+        var elementSize = AppConfig.DEFAULT_ELEMENT_SIZE
+        var elementSpacing = AppConfig.DEFAULT_ELEMENT_SPACING
+        var elementAnimationDelay = AppConfig.DEFAULT_ELEMENT_ANIMATION_DELAY
+        var numOfElements = AppConfig.DEFAULT_ELEMENT_NUM
     }
 
     private lateinit var gridAdapter: AutoFitRecyclerAdapter
@@ -23,21 +22,24 @@ class GridActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grid)
 
-        gridAdapter = AutoFitRecyclerAdapter(ArrayList((1..20).map {
+        gridAdapter = AutoFitRecyclerAdapter(ArrayList((1..numOfElements).map {
             it
         }))
+
         val itemAnimator = DefaultItemAnimator()
-        itemAnimator.moveDuration = 1000
-        gridRecyclerView.itemAnimator = itemAnimator
-        gridRecyclerView.layoutManager = GridLayoutManager(this, calculateColumns())
-        gridRecyclerView.adapter = gridAdapter
+        itemAnimator.moveDuration = elementAnimationDelay.toLong()
+
+        gridRecyclerView.apply {
+            this.itemAnimator = itemAnimator
+            this.layoutManager = GridLayoutManager(this@GridActivity, calculateColumns())
+            this.adapter = gridAdapter
+        }
     }
 
     private fun calculateColumns(): Int {
         val displayMetrics = resources.displayMetrics
         val density = displayMetrics.density
         val width: Int = displayMetrics.widthPixels
-        Log.d("MainActivity", "$density $width $elementSize")
         var columns = (width / (elementSize * density)).toInt()
         if (columns == 0 || columns == 1) columns = 2
         return columns
